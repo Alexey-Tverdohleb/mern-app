38ae0bbcc6066fd1c1125aa1fb070f9c347d4ee0
AlexeyTverdohleb <allexey.tverdohleb@gmail.com>
2023-07-20 16:10:11 +0300

    display header links according to auth status

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,16 +1,24 @@
 import Container from '../Container/index.jsx';
 import { NavHeader, NavRow, NavLinkItem } from './style.js';
+import useAuth from '../../context/AuthContext/useAuth.js';
+import { privateLinks, publicLinks } from './links';
+import AUTH_STATUS from '../../constants/auth';
 
 const Header = () => {
+  const authStatus = useAuth();
+  const navLinks = authStatus === AUTH_STATUS.authed ? privateLinks : publicLinks;
+
   return (
     <NavHeader>
       <Container>
         <NavRow>
           <h2>MERN</h2>
           <nav>
-            <NavLinkItem to="/">Home</NavLinkItem>
-            <NavLinkItem to="login">Login</NavLinkItem>
-            <NavLinkItem to="registration">Registration</NavLinkItem>
+            {navLinks.map(({ path, label }) => (
+              <NavLinkItem key={path} to={path}>
+                {label}
+              </NavLinkItem>
+            ))}
           </nav>
         </NavRow>
       </Container>
---
+++ client/src/components/Header/links.js
@@ -0,0 +1,25 @@
+export const privateLinks = [
+  {
+    path: '/',
+    label: 'Home',
+  },
+  {
+    path: 'dashboard',
+    label: 'Dashboard',
+  },
+  {
+    path: 'management',
+    label: 'Management',
+  },
+];
+
+export const publicLinks = [
+  {
+    path: 'login',
+    label: 'Login',
+  },
+  {
+    path: 'registration',
+    label: 'Registration',
+  },
+];
