cbb6d18455cffe67e53e74b7adb22a2c3b05a90a
AlexeyTverdohleb <allexey.tverdohleb@gmail.com>
2023-07-20 12:27:46 +0300

    set auth token in request headers

--- client/src/api/client.js
+++ client/src/api/client.js
@@ -1,7 +1,19 @@
 import ky from 'ky';
 
+import { TOKEN } from '../constants/localStorage.js';
+
 const api = ky.extend({
   prefixUrl: import.meta.env.VITE_API_URL,
+  hooks: {
+    beforeRequest: [
+      (request) => {
+        const token = window.localStorage.getItem(TOKEN);
+        if (!token) return;
+
+        request.headers.set('Authorization', `Bearer ${token}`);
+      },
+    ],
+  },
 });
 
 export default api;
