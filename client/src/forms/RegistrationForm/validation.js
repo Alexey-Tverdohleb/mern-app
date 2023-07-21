53a1338643db8d8b208133d282d44dbcc31c0d46
AlexeyTverdohleb <allexey.tverdohleb@gmail.com>
2023-07-21 17:18:51 +0300

    add password and password repeat matching validation

--- client/src/forms/RegistrationForm/validation.js
+++ client/src/forms/RegistrationForm/validation.js
@@ -7,8 +7,12 @@ const validationScheme = yup.object().shape({
     .string()
     .required('Password is required.')
     .min(6, 'Password must have 6 characters at least.')
-    .max(16, 'Password should not have more than 16 characters.'),
-  passwordRepeat: yup.string().required('Repeat password is required.'),
+    .max(16, 'Password should not have more than 16 characters.')
+    .oneOf([null, yup.ref('passwordRepeat')], 'Password and password repeat do not match.'),
+  passwordRepeat: yup
+    .string()
+    .required('Repeat password is required.')
+    .oneOf([null, yup.ref('password')], 'Password and password repeat do not match.'),
 });
 
 export default validationScheme;
